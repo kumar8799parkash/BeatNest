c808031357c50baebb74af278e3054352e8b66e0
kumar8799parkash <kumar8799parkash@gmail.com>
2025-08-22 21:45:33 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
-const express = require('express') 
+const express = require('express')
+const jwt = require('jsonwebtoken')         // To create login session tokens
 const bcrypt = require('bcrypt')            // bcrypt is a password-hashing function designed for securely storing passwords.
 const dotenv = require('dotenv')            // Hide secrets (DB passwords, API keys, JWT secrets).
 const mongoose = require('mongoose')        // Mongoose allows you to define a schema, mongoDB is itself schema-less(no defined structure)
@@ -6,45 +7,71 @@ const app = express()
 const port = 3000
 
 dotenv.config();                            // This line reads your .env file and adds the variables inside it to process.env
-                                            // So after dotenv.config(), you can use process.env.MY_VARIABLE anywhere in your app.
+// So after dotenv.config(), you can use process.env.MY_VARIABLE anywhere in your app.
 
 app.use(express.json())                     // Middleware to parse JSON data from frontend
 
 
-async function connectDB(){
-    const uri = process.env.MONGO_URI;
-    if(!uri) throw new Error("MONGO_URI does not exists in secrets.env file!");
+async function connectDB() {
+  const uri = process.env.MONGO_URI;
+  if (!uri) throw new Error("MONGO_URI does not exists in .env file!");
 
-    try{
-        await mongoose.connect(uri);
-        console.log("database connected");
-    }
-    catch(err){
-        console.log("mongoDB connection error!" , err.message);
-    }
-    
+  try {
+    await mongoose.connect(uri);
+    console.log("database connected");
+  }
+  catch (err) {
+    console.log("mongoDB connection error!", err.message);
+  }
 }
 
 connectDB();
 
 const userSchema = new mongoose.Schema({
-  email : {type:String , required:true , unique:true},
-  password : {type:String},
-  googleId : {type:String}
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  googleId: { type: String }
 });
-const User = mongoose.model("User" , userSchema);
+const User = mongoose.model("User", userSchema);
 
-app.post('/signup' , async (req , res)=>{
-  const {email , password} = req.body;
-  const existingUser = await User.findOne({email});
-  if(existingUser){
-    return  res.status(400).json({msg : "User already exists!"});
+app.post('/signup', async (req, res) => {              // remember that here we have declared it as a ASYNC function so we CAN USE AWAIT
+
+  try {
+    const { email, password } = req.body;
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ msg: "User already exists!" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);          // hashing the password
+    const newUser = new User({ email: email, password: hashedPassword });
+    await newUser.save();
+
+    const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, { expiresIn: "1h" });
+    res.json({ token, user: { id: newUser._id, email: newUser.email } });
+  } catch (err) {
+    res.status(500).json({ error: "Error during signUp!" , details: err.message })
   }
 
-  const hashedPassword = await bcrypt.hash(password , 10);          // hashing the password
-  const newUser = new User({email , password : hashedPassword});
-  await newUser.save();
+});
 
+
+app.post('/login', async (req, res) => {                 // remember that here we have declared it as a ASYNC function so we CAN USE AWAIT
+
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) return res.status(400).json({ msg: "User does not exist!" })
+
+    // if user exists the check if the current password(password) is equal to the user.password
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).json({ msg: "Invalid credentials!" });
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "1h" });
+    res.json({ token, user: { id: user._id, email: user.email } });
+  }catch(err){
+    res.status(500).json({error : "Error in login logic!" , details: err.message})
+  }
 
 })
 
