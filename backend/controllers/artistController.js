1f8249278db05eecacbf2773dac3dc1d990c81d2
kumar8799parkash <kumar8799parkash@gmail.com>
2025-10-24 13:03:42 +0530

    upadated

--- backend/controllers/artistController.js
+++ backend/controllers/artistController.js
@@ -4,7 +4,7 @@ const Song = require('../models/song');
 
 exports.getAllArtists = async (req, res) => {
     try {
-        const artists = await Artist.find({});
+        const artists = await Artist.find({} , 'name image _id');
         res.json(artists);
     } catch (err) {
         return res.status(500).json({ msg: "error occured while fetching all the artists inside artistController.js", error: err.message });
